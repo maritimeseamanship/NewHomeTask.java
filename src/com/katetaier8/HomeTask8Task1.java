15fe75339531f0ea17550e68e9253150b7c7b7b9
maritimeseamanship <ekaterinataer@gmail.com>
2022-01-24 20:51:37 +0200

    +Task2

--- src/com/katetaier8/HomeTask8Task1.java
+++ src/com/katetaier8/HomeTask8Task1.java
@@ -11,6 +11,12 @@ public class HomeTask8Task1 {
         int count = getCount();
         System.out.println(count);
 
+        /*Найти количество счастливых билетиков на трамвай от 000001 до 999999 - те у которых сумма первых 3 цифр
+          равна сумме последних 3*/
+        int countTickets = 0;
+        System.out.println("The number of happy tickets is " + happyTickets(countTickets) + ".");
+
+
     }
 
     private static int getCount() {
@@ -24,6 +30,17 @@ public class HomeTask8Task1 {
             }
         }
         return count;
+    }
+
+    private static int happyTickets(int countTickets) {
+        for (int i = 0; i <= 999; i++) {
+            for (int j = 0; j <= 999; j++) {
+                if (i / 100 + i / 10 % 10 + i % 10 == j / 100 + j / 10 % 10 + j % 10) {
+                    countTickets++;
+                }
+            }
+        }
+        return countTickets;
     }
 }
 
